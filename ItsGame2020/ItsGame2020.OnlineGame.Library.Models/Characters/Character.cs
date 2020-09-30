e372c2c80060a55af8dda346d53989ee319fb860
Luca <Luca@ALVAROPC>
2020-09-30 14:23:07 +0200

    Script iniziale Prof

--- ItsGame2020/ItsGame2020.OnlineGame.Library.Models/Characters/Character.cs
+++ ItsGame2020/ItsGame2020.OnlineGame.Library.Models/Characters/Character.cs
@@ -7,12 +7,20 @@ namespace ItsGame2020.OnlineGame.Library.Models.Characters
     public abstract class Character
     {
         public string Name { get; set; } = "Player";
-        public bool Gender { get; set; }
-        public CharacterType Type { get; set; }
-    }
-    public class Wizard : Character
-    {
+        public Gender Gender { get; set; }
+        public abstract int MaxHealth { get; }
+        public int HealthPoints { get; set; } = 100;
+        public int ManaPoints { get; set; }
+        public int Level { get; } = 1;
+        public int ExpLevelUp { get; set; }
 
+        public void Heal(int amount)
+        {
+            HealthPoints += amount;
+            if (HealthPoints > MaxHealth)
+                HealthPoints = MaxHealth;
+        }
+        public CharacterType Type { get; set; }
     }
 
     public enum CharacterType
@@ -24,9 +32,23 @@ namespace ItsGame2020.OnlineGame.Library.Models.Characters
 
     public enum Gender
     {
-        Unknown,
         Male,
-        Female
+        Female,
+        Unknown
     }
 
+    public class Wizard : Character
+    {
+        public override int MaxHealth => 100;
+    }
+
+    public class Rogue : Character
+    {
+        public override int MaxHealth => 110;
+    }
+
+    public class Warrior : Character
+    {
+        public override int MaxHealth => 130;
+    }
 }
--- ItsGame2020/ItsGame2020/Program.cs
+++ ItsGame2020/ItsGame2020/Program.cs
@@ -1,24 +1,29 @@
-﻿using System;
-using System.Reflection.PortableExecutable;
-using System.Runtime.InteropServices;
-using ItsGame2020.OnlineGame.Library.Models;
-using ItsGame2020.OnlineGame.Library.Models.Characters;
+﻿using ItsGame2020.OnlineGame.Library.Models.Characters;
 
-namespace ItsGame2020.Game
+using System;
+using System.Collections.Generic;
+
+namespace Test.ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Character character = new Wizard();
-            PrintCharacters(new Wizard());
-        }
+            List<Character> characters = new List<Character>();
+            Warrior warrior = new Warrior();
+            Wizard wizard = new Wizard();
+            Rogue rogue = new Rogue();
+            characters.Add(warrior);
+            characters.Add(wizard);
+            characters.Add(rogue);
 
-        private static void PrintCharacters(Character character )
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(character.Name);
-            Console.ResetColor();
+            foreach (Character character in characters)
+            {
+                Console.WriteLine($"{character.GetType().Name} Heath = {character.HealthPoints}");
+                character.Heal(100);
+                Console.WriteLine($"{character.GetType().Name} Heath = {character.HealthPoints}");
+
+            }
         }
     }
 }
